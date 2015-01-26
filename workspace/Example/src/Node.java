1d72ce0796ff3c05859389d26198ca410e791e7f
Joseph Lawrance <lawrancej@wit.edu>
2015-01-26 14:32:02 -0500

    Node modifications

--- workspace/Example/src/Node.java
+++ workspace/Example/src/Node.java
@@ -1,17 +1,26 @@
 
 public class Node<Type> {
 	private Type data;
-	private Node<Type> link;
+	private Node<Type> next;
+	private Node<Type> prev;
 	public Type getData() {
 		return data;
 	}
 	public void setData(Type data) {
 		this.data = data;
 	}
-	public Node<Type> getLink() {
-		return link;
+	public Node<Type> getNext() {
+		return next;
 	}
-	public void setLink(Node<Type> link) {
-		this.link = link;
+	public void setNext(Node<Type> node /* thing2 */) {
+		this.next = node; // thing1.next = thing2
+		node.prev = this; // thing2.prev = thing1
+	}
+	public Node<Type> getPrev() {
+		return prev;
+	}
+	public void setPrev(Node<Type> node /* thing0 */) {
+		this.prev = node; // thing1.prev = thing0
+		node.next = this; // thing0.next = thing1
 	}
 }
--- workspace/Example/src/NodeTest.java
+++ workspace/Example/src/NodeTest.java
@@ -7,8 +7,13 @@ public class NodeTest {
 
 	@Test
 	public void test() {
-		Node<String> stringNode = new Node<String>();
-		Node<Integer> intNode = new Node<Integer>();
+		Node<Integer> thing1 = new Node<Integer>();
+		Node<Integer> thing2 = new Node<Integer>();
+		thing1.setData(3);
+		thing2.setData(10);
+		thing1.setNext(thing2);
+		assertEquals(thing2, thing1.getNext());
+		assertEquals(thing1, thing2.getPrev());
 	}
 
 }
