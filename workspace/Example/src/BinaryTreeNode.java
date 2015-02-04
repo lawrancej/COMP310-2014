bd5b51d569208bd7aade3fb477af01d11abdc207
Joey Lawrance <lawrancej@wit.edu>
2015-02-04 14:32:04 -0500

    implement contains, ftw.

--- workspace/Example/src/BinaryTreeNode.java
+++ workspace/Example/src/BinaryTreeNode.java
@@ -7,25 +7,32 @@ public class BinaryTreeNode
 	BinaryTreeNode(DataType data) {
 		this.data = data;
 	}
-	
 	void add(DataType moreData) {
 		// figure out which side to place it
 		if (data.compareTo(moreData) <= 0) {
 			// it goes to the right
 			if (right == null) {
 				// create a node with the data and call it a day
+				right = new BinaryTreeNode<DataType>(moreData);
 			} else {
 				// keep looking recursively
+				right.add(moreData);
 			}
 		} else {
 			// it goes to the left
 			if (left == null) {
 				// create a node and call it a day
+				left = new BinaryTreeNode<DataType>(moreData);
 			} else {
 				// keep looking recursively
+				left.add(moreData);
 			}
 		}
-		// put it there
+	}
+	// Can you write this function?
+	// Hint: try doing what you do in the add function
+	boolean contains (DataType moreData) {
+		return false;
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello".compareTo("World"));
---
+++ workspace/Example/src/BinaryTreeNodeTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BinaryTreeNodeTest {
+
+	@Test
+	public void testAdd() {
+		BinaryTreeNode<String> tree =
+				new BinaryTreeNode<String>("dog");
+		tree.add("cat");
+		tree.add("fish");
+		tree.add("turtle");
+		tree.add("snake");
+		assertEquals("cat", tree.left.data);
+		assertEquals("fish", tree.right.data);
+		assertEquals("turtle", tree.right.right.data);
+		assertEquals("snake", tree.right.right.left.data);
+		assertTrue(tree.contains("snake"));
+	}
+
+}
